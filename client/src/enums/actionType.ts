b34cce140538757da7cd6e15a6fc649dc7ba88aa
Casey Schmid <caseywschmid@gmail.com>
2025-05-15 12:13:51 +0200

    updated ActionType frontend enum

--- client/src/enums/actionType.ts
+++ client/src/enums/actionType.ts
@@ -1,11 +1,12 @@
 /**
  * Type of action to perform
  */
-export type ActionType =
-  | "create_identity"
-  | "update_identity"
-  | "accept_identity"
-  | "accept_identity_refinement"
-  | "add_identity_note"
-  | "transition_state"
-  | "select_identity_focus";
+export enum ActionType {
+  CREATE_IDENTITY = "create_identity",
+  UPDATE_IDENTITY = "update_identity",
+  ACCEPT_IDENTITY = "accept_identity",
+  ACCEPT_IDENTITY_REFINEMENT = "accept_identity_refinement",
+  ADD_IDENTITY_NOTE = "add_identity_note",
+  TRANSITION_STATE = "transition_state",
+  SELECT_IDENTITY_FOCUS = "select_identity_focus",
+}
