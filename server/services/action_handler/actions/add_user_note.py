45644ce3b5e6f4d48e4dab61b723a176cdf5db29
Casey Schmid <caseywschmid@gmail.com>
2025-08-03 19:28:00 +0200

    Updated add_user_note function to include a note about the logging behavior, clarifying that actions performed by the Sentinel do not belong with "Coach" actions.

--- server/services/action_handler/actions/add_user_note.py
+++ server/services/action_handler/actions/add_user_note.py
@@ -3,6 +3,8 @@ from services.action_handler.models import AddUserNoteParams
 from apps.coach_states.models import CoachState
 
 
+# NOTE: For now, this function does not log actions to the Action table.
+# Since these actions are performed by the Sentinel only, these actions don't belong with the "Coach" actions
 def add_user_note(coach_state: CoachState, params: AddUserNoteParams):
     """
     Action handler to add new notes about the user.
