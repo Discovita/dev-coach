c90aebd06d546b215c24350fd08fc7b9fe9fa205
Casey Schmid <caseywschmid@gmail.com>
2025-08-07 16:05:50 +0200

    corrected IW name to match the back end

--- client/src/enums/coachingPhase.ts
+++ client/src/enums/coachingPhase.ts
@@ -7,7 +7,7 @@ export enum CoachingPhase {
   SYSTEM_CONTEXT = "system_context",
   INTRODUCTION = "introduction",
   GET_TO_KNOW_YOU = "get_to_know_you",
-  IDENTITY_WARMUP = "identity_warmup",
+  IDENTITY_WARMUP = "identity_warm_up",
   IDENTITY_BRAINSTORMING = "identity_brainstorming",
   IDENTITY_REFINEMENT = "identity_refinement",
 }
