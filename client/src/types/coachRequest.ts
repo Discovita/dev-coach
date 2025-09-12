9b739154a2496eb09816af4300833d0fb697f204
Casey Schmid <caseywschmid@gmail.com>
2025-09-12 12:28:10 +0200

    updated the coachRequest model to match the back end serializer

--- client/src/types/coachRequest.ts
+++ client/src/types/coachRequest.ts
@@ -1,3 +1,5 @@
+import { Action } from "./action";
+
 /**
  * Request model for coach API.
  */
@@ -6,5 +8,13 @@ export interface CoachRequest {
    * User's message
    */
   message: string;
-  model?: string;
+  /**
+   * Optional model name. If not provided, uses default.
+   */
+  model_name?: string;
+  /**
+   * List of actions to execute in order. Each item should be an object
+   * with 'action' (str) and 'params' (object). Can be sent alongside message.
+   */
+  actions?: Action[];
 }
