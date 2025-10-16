b043981022c31e31899d90010031a973290dbb5c
Casey Schmid <caseywschmid@gmail.com>
2025-10-16 19:20:30 +0200

    added util to set the first current identity when moving to IR

---
+++ server/services/action_handler/utils/__init__.py
@@ -0,0 +1,11 @@
+from .set_current_identity_to_first_pending_refinement import set_current_identity_to_first_pending_refinement
+from .update_all_user_identities_to_accepted_state import update_all_user_identities_to_accepted_state
+from .dynamic_schema import build_dynamic_response_format
+from .action_instructions import get_action_instructions
+
+__all__ = [
+    "set_current_identity_to_first_pending_refinement",
+    "update_all_user_identities_to_accepted_state",
+    "build_dynamic_response_format",
+    "get_action_instructions",
+]
