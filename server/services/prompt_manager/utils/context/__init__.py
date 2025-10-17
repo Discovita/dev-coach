9c87cf569fd88fa132d8a4048d2fc6c13fb6243b
Casey Schmid <caseywschmid@gmail.com>
2025-10-17 13:28:50 +0200

    updated prompt manager util exports

--- server/services/prompt_manager/utils/context/__init__.py
+++ server/services/prompt_manager/utils/context/__init__.py
@@ -1,7 +1,25 @@
 from .gather_prompt_context import gather_prompt_context
 from .get_context_value import get_context_value
-
+from .func import *
 __all__ = [
     "gather_prompt_context",
     "get_context_value",
+    "get_user_name_context",
+    "get_identities_context",
+    "get_number_of_identites_context",
+    "get_identity_focus_context",
+    "get_recent_messages_context",
+    "get_who_you_are",
+    "get_who_you_want_to_be",
+    "get_focused_identities_context",
+    "get_user_notes_context",
+    "get_current_message_context",
+    "get_previous_message_context",
+    "get_current_phase_context",
+    "get_brainstorming_category_context",
+    "get_current_identity_context",
+    "get_asked_questions",
+    "get_refinement_identities_context",
+    "get_affirmation_identities_context",
+    "get_visualization_identities_context",
 ]
